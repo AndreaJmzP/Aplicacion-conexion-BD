0dc2e7ca4d911691a8e7b03969a2029af2bd60c1
AndreaJmzP <andreajimenezp.0582@gmail.com>
2025-05-30 09:34:39 -0600

    ...

--- src/services/actor.service.ts
+++
@@ -1,7 +0,0 @@
-import { ActorRepository } from '../repositories/actor.repos.ts';
-
-export const ActorService = {
-    getAll: () => ActorRepository.findAll(),
-    getById: (id: number) => ActorRepository.findById(id),
-    add: (data: { first_name: string; last_name: string }) => ActorRepository.add(data)
-};
